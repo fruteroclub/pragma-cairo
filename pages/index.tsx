0913a8c581c6fc3cc1252640fddb223c42b7bfe4
haycarlitos <carlos.castilloperera@gmail.com>
2023-06-20 20:04:33 -0600

    add wido

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import { WidoWidget } from 'wido-widget'
+import { useEffect, useState } from 'react'
+import { getSupportedTokens } from 'wido'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [fromTokens, setFromTokens] = useState<{ chainId: number; address: string }[]>([])
+  const [toTokens, setToTokens] = useState<{ chainId: number; address: string }[]>([])
+
+  useEffect(() => {
+      getSupportedTokens({ chainId: [1] }).then(setFromTokens)
+      getSupportedTokens({ chainId: [15366], protocol: ['jediswap.xyz'] }).then(setToTokens)
+  }, [setFromTokens, setToTokens])
   return (
+    
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <WidoWidget
+ 
+    />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
