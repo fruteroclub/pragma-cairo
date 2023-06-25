40d2db05d07b2652c888b6b79807797b9b701caa
haycarlitos <carlos.castilloperera@gmail.com>
2023-06-25 15:54:42 -0600

    change route name

--- pages/starkVault/index.tsx
+++ pages/starkVault/index.tsx
@@ -193,6 +193,7 @@ function App() {
             </dl>
         </div>
         </section>
+        {/*
         <section className="bg-white dark:bg-gray-900">
         <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
             <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-4 dark:text-white">
@@ -214,7 +215,7 @@ function App() {
                 </div>
             </dl>
         </div>
-        </section>
+        </section>*/}
     </div>
     
   );
